eb021c05e2c76a416ba0020bb87fd72ed792a09b
stsg732 <1034142391@qq.com>
2024-09-03 09:51:39 +0800

    Update section_3.py

--- section_3.py
+++ section_3.py
@@ -148,7 +148,7 @@ st.subheader('Datetime slider')
 
 start_time = st.slider(
     "When do you start?",
-    value = datetime(2020,1,1,9,30),  # 默认值设置为2020年1月1日9点30分
+    value = datetime(2020,1,1,9,30),  
     format = "MM/DD/YY - hh:mm"
 )
 st.write("Start time:",start_time)
